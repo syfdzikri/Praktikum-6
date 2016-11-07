8923f354c09cab9af9558cb002cefa326ca16591
Syifa Dzikri Tsani <syifadzikri@gmail.com>
2016-11-07 17:18:43 +0700

    Add files via upload

---
+++ Praktikum_6-2.ino
@@ -0,0 +1,51 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(2,3,4,5,6,7);
+unsigned int jam=14, menit=58, detik=45;
+boolean led = 0;
+ISR(TIMER1_OVF_vect) {
+  TCNT1H=0xC2;
+  TCNT1L=0xF7;
+  detik++;
+  if(detik>=60) {
+    detik = 0;
+    menit++;
+    if(menit>=60) {
+      menit = 0;
+      jam++;
+      if(jam>=24) {
+        jam = 0;
+      }
+    }
+  }
+}
+void initTimer1() {
+  TCCR1A=0x00;
+  TCCR1B=0x05;
+  TCNT1H=0xC2;
+  TCNT1L=0xF7;
+  ICR1H=0x00;
+  ICR1L=0x00;
+  OCR1AH=0x00;
+  OCR1AL=0x00;
+  OCR1BH=0x00;
+  OCR1BL=0x00;
+  TIMSK1=0x01;
+  sei();
+}
+void setup() {
+  lcd.begin(16,2);
+  lcd.clear();
+  initTimer1();
+}
+void loop() {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Jam Digital");
+  lcd.setCursor(0, 1);
+  lcd.print(jam,DEC);
+  lcd.print(":");
+  lcd.print(menit,DEC);
+  lcd.print(":");
+  lcd.print(detik,DEC);
+  delay(500);
+}
---
+++ Praktikum_6-3.ino
@@ -0,0 +1,18 @@
+void setup() {
+  Serial.begin(9600);
+  TCCR1A=0x00;
+  TCCR1B=0x06;
+  TCNT1H=0x00;
+  TCNT1L=0x00;
+  ICR1H=0x00;
+  ICR1L=0x00;
+  OCR1AH=0x00;
+  OCR1AL=0x00;
+  OCR1BH=0x00;
+  OCR1BL=0x00;
+  }
+  void loop() {
+    Serial.print("Nilai TCNT1 = ");
+    Serial.println(TCNT1,DEC);
+    delay(100);
+}
